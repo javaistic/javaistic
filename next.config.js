087f7cdc143bc38465872f3c9cc0c1e01ae6621e
Arghya Ghosh <71373838+uiuxarghya@users.noreply.github.com>
2021-05-13 03:56:47 +0530

    added next config

---
+++ next.config.js
@@ -0,0 +1,11 @@
+const withNextra = require('nextra')({
+  theme: 'nextra-theme-docs', 
+  themeConfig: './theme.config.js',
+})
+
+module.exports = withNextra({
+  i18n: {
+    locales: ['en-US'],
+    defaultLocale: 'en-US'
+  }
+})
