2e95366a5c4b6f1dd15fb1ab4797163766334fd6
Arghya Ghosh <71373838+uiuxarghya@users.noreply.github.com>
2021-06-11 17:39:23 +0530

    added Google Analytics

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,34 @@
-import React from 'react'
-import Document, { Html, Head, Main, NextScript } from 'next/document'
-import { SkipNavLink } from '@reach/skip-nav'
+import React from "react";
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import { SkipNavLink } from "@reach/skip-nav";
 
 class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/docsearch.js@2/dist/cdn/docsearch.min.css" />
+        <Head>
+          <link
+            rel="stylesheet"
+            href="https://cdn.jsdelivr.net/npm/docsearch.js@2/dist/cdn/docsearch.min.css"
+          />
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
+        </Head>
         <body>
           <SkipNavLink />
           <Main />
@@ -19,8 +40,8 @@ class MyDocument extends Document {
           />
         </body>
       </Html>
-    )
+    );
   }
 }
 
-export default MyDocument
+export default MyDocument;
