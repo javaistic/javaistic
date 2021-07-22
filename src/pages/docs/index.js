aaf6ec7b1850683221f065bd37925c6555a03284
Arghya Ghosh <71373838+uiuxarghya@users.noreply.github.com>
2021-07-23 03:41:46 +0530

    Add docs page

---
+++ src/pages/docs/index.js
@@ -0,0 +1,83 @@
+import { Community } from '@/components/Community'
+import { ReactComponent as GuidesImage } from '@/img/guides.svg'
+import { ReactComponent as PlayImage } from '@/img/play.svg'
+import { DocumentationLayout } from '@/layouts/DocumentationLayout'
+import { gradients } from '@/utils/gradients'
+import Link from 'next/link'
+import styles from './index.module.css'
+
+export default function DocsLandingPage() {
+  return (
+    <div className="px-4 sm:px-6 xl:px-8 pt-10 pb-16">
+      <h1 className="text-5xl leading-none font-extrabold text-gray-900 tracking-tight mb-4">
+        Getting started with Java
+      </h1>
+      <p className="text-2xl tracking-tight mb-10">
+        Learn Java the way that best matches your learning style.
+      </p>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 xl:gap-8">
+        <section className="flex">
+          <div className="w-full relative text-white overflow-hidden rounded-3xl flex shadow-lg">
+            <div className={`w-full flex md:flex-col bg-gradient-to-br ${gradients.violet[0]}`}>
+              <div className="sm:max-w-sm sm:flex-none md:w-auto md:flex-auto flex flex-col items-start relative z-10 p-6 xl:p-8">
+                <h2 className="text-xl font-semibold mb-2 text-shadow">Read the docs</h2>
+                <p className="font-medium text-violet-100 text-shadow mb-4">
+                  Learn how to get Java set up in your project.
+                </p>
+                <Link href="/docs/installation">
+                  <a className="mt-auto bg-violet-800 bg-opacity-50 hover:bg-opacity-75 transition-colors duration-200 rounded-xl font-semibold py-2 px-4 inline-flex">
+                    Start learning
+                  </a>
+                </Link>
+              </div>
+              <div className={`${styles.image} relative md:pl-6 xl:pl-8 hidden sm:block`}>
+                <GuidesImage className="absolute top-6 left-6 md:static overflow-visible" />
+              </div>
+            </div>
+            <div
+              className="absolute bottom-0 left-0 right-0 h-20 hidden sm:block"
+              style={{
+                background: 'linear-gradient(to top, rgb(135, 94, 245), rgba(135, 94, 245, 0))',
+              }}
+            />
+          </div>
+        </section>
+        <section className="flex">
+          <div className="w-full relative text-white overflow-hidden rounded-3xl flex shadow-lg">
+            <div className={`w-full flex md:flex-col bg-gradient-to-br ${gradients.pink[0]}`}>
+              <div className="sm:max-w-sm sm:flex-none md:w-auto md:flex-auto flex flex-col items-start relative z-10 p-6 xl:p-8">
+                <h2 className="text-xl font-semibold mb-2 text-shadow">Java Programs</h2>
+                <p className="font-medium text-rose-100 text-shadow mb-4">
+                  Start coding right now or go through Java code.
+                </p>
+                <a
+                  href="/programs"
+                  className="mt-auto bg-rose-900 bg-opacity-50 hover:bg-opacity-75 transition-colors duration-200 rounded-xl font-semibold py-2 px-4 inline-flex"
+                >
+                  Start coding
+                </a>
+              </div>
+              <div className={`${styles.image} relative md:pl-6 xl:pl-8 hidden sm:block`}>
+                <PlayImage className="absolute top-6 left-6 md:static overflow-visible" />
+              </div>
+            </div>
+            <div className="absolute bottom-0 left-0 right-0 h-20 bg-gradient-to-t from-rose-500 hidden sm:block" />
+          </div>
+        </section>
+      </div>
+      <section>
+        <h2 className="text-3xl tracking-tight font-extrabold text-gray-900 mt-16 mb-8">
+          Get involved
+        </h2>
+        <Community />
+      </section>
+    </div>
+  )
+}
+
+DocsLandingPage.layoutProps = {
+  meta: {
+    title: 'Documentation',
+  },
+  Layout: DocumentationLayout,
+}
