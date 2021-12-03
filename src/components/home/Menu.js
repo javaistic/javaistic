767933423e16f929b40aa0deae24f0613cebc0ee
Arghya Ghosh <71373838+uiuxarghya@users.noreply.github.com>
2021-12-04 03:15:12 +0530

    Update Menu.js

--- src/components/home/Menu.js
+++ src/components/home/Menu.js
@@ -30,7 +30,7 @@ export default function MenuButton() {
                         <Menu.Item>
                             {({ active }) => (
                                 <a
-                                    href="#"
+                                    href="/docs"
                                     className={classNames(
                                         active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
                                         'block px-4 py-2 text-base sm:hidden'
