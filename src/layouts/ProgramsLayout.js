917b6b05d3f5208a9beb86ffeb47e6a6c8b07f13
Arghya Ghosh <71373838+uiuxarghya@users.noreply.github.com>
2021-12-21 03:17:56 +0530

    Update ProgramsLayout.js

--- src/layouts/ProgramsLayout.js
+++ src/layouts/ProgramsLayout.js
@@ -15,11 +15,12 @@ export function ProgramsLayout(props) {
       </Title>
       <Head>
         <meta name="description" content={props.layoutProps.meta.metaDescription || props.layoutProps.meta.description} />
+        <meta key="og:image" property="og:image" content={router.pathname === '/programs' ? `https://javaistic.vercel.app${twitterPrograms}`:`https://javaistic-og-image.vercel.app/**${props.layoutProps.meta.metaTitle || props.layoutProps.meta.title}**.png?theme=light&md=1&fontSize=120px&images=https%3A%2F%2Fjavaistic-assets.vercel.app%2Flogo%2Fjavaistic-logo-main.svg`} />
         <meta key="twitter:card" name="twitter:card" content="summary_large_image" />
         <meta
           key="twitter:image"
           name="twitter:image"
-          content={`https://javaistic.vercel.app${twitterPrograms}`}
+          content={router.pathname === '/programs' ? `https://javaistic.vercel.app${twitterPrograms}` : `https://javaistic-og-image.vercel.app/**${props.layoutProps.meta.metaTitle || props.layoutProps.meta.title}**.png?theme=light&md=1&fontSize=120px&images=https%3A%2F%2Fjavaistic-assets.vercel.app%2Flogo%2Fjavaistic-logo-main.svg`}
         />
       </Head>
       <SidebarLayout nav={programsNav} {...props} />
