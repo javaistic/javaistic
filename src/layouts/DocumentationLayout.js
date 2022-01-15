a31e88f04bd8f7b8c4dbb76be162b77a27e6076b
Arghya Ghosh <71373838+uiuxarghya@users.noreply.github.com>
2022-01-16 03:25:33 +0530

    Update DocumentationLayout.js
    Update og-image link.

--- src/layouts/DocumentationLayout.js
+++ src/layouts/DocumentationLayout.js
@@ -16,11 +16,11 @@ export function DocumentationLayout(props) {
       <Head>
         <meta name="description" content={props.layoutProps.meta.metaDescription || props.layoutProps.meta.description} />
         <meta key="twitter:card" name="twitter:card" content="summary_large_image" />
-        <meta key="og:image" property="og:image" content={router.pathname === '/docs' ? `https://javaistic.vercel.app${twitterDocs}`:`https://javaistic-og-image.vercel.app/**${props.layoutProps.meta.metaTitle || props.layoutProps.meta.title}**.png?theme=light&md=1&fontSize=120px&images=https%3A%2F%2Fjavaistic-assets.vercel.app%2Flogo%2Fjavaistic-logo-main.svg`} />
+        <meta key="og:image" property="og:image" content={router.pathname === '/docs' ? `https://javaistic.vercel.app${twitterDocs}`:`https://javaistic-og-image.vercel.app/**${props.layoutProps.meta.metaTitle || props.layoutProps.meta.title}**.png?theme=light&md=1&fontSize=120px&images=https%3A%2F%2Fjavaistic-assets.vercel.app%2Flogo%2Fjavaistic-docs-main.svg`} />
         <meta
           key="twitter:image"
           name="twitter:image"
-          content={router.pathname === '/docs' ? `https://javaistic.vercel.app${twitterDocs}` : `https://javaistic-og-image.vercel.app/**${props.layoutProps.meta.metaTitle || props.layoutProps.meta.title}**.png?theme=light&md=1&fontSize=120px&images=https%3A%2F%2Fjavaistic-assets.vercel.app%2Flogo%2Fjavaistic-logo-main.svg`}
+          content={router.pathname === '/docs' ? `https://javaistic.vercel.app${twitterDocs}` : `https://javaistic-og-image.vercel.app/**${props.layoutProps.meta.metaTitle || props.layoutProps.meta.title}**.png?theme=light&md=1&fontSize=120px&images=https%3A%2F%2Fjavaistic-assets.vercel.app%2Flogo%2Fjavaistic-docs-main.svg`}
         />
       </Head>
       <SidebarLayout nav={documentationNav} {...props} />
