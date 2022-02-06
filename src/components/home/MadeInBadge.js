a3f12990447191386af0039f2c572a651355b9a5
Arghya Ghosh <71373838+uiuxarghya@users.noreply.github.com>
2022-02-07 00:58:49 +0530

    Update MadeInBadge.js

--- src/components/home/MadeInBadge.js
+++ src/components/home/MadeInBadge.js
@@ -3,7 +3,7 @@ import { IndianFlag } from '@/components/Logos'
 export default function MadeInBadge() {
   return (
     <div className="title-font flex items-center justify-center pl-6 font-medium text-gray-900 md:justify-start">
-      <div className="mt-2 flex items-center text-sm text-gray-500">
+      <div className="mt-2 flex items-center text-sm text-gray-500 dark:text-gray-300">
         Made with
         <div className="heart mx-2"></div>
         in
