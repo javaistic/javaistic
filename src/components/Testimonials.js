661ec606d21c33eeb5c3d9c9a26274c719c85e1e
Arghya Ghosh <71373838+uiuxarghya@users.noreply.github.com>
2022-05-17 21:46:11 +0530

    Update Testimonials.js

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -108,12 +108,12 @@ function Testimonial({ testimonial, base, index, total }) {
         initial={false}
         animate={straight ? { rotate: 0 } : { rotate: rotation[index % rotation.length] }}
       >
-        <blockquote className="rounded-t-xl bg-white px-6 py-8 text-lg font-semibold leading-8 text-gray-900 md:p-10 md:text-xl md:leading-8">
+        <blockquote className="rounded-t-xl bg-white px-6 py-8 text-lg font-semibold leading-8 text-gray-900 dark:bg-gray-800 md:p-10 md:text-xl md:leading-8">
           <svg width="45" height="36" className={`mb-5 fill-current ${color[1]}`}>
             <path d="M13.415.001C6.07 5.185.887 13.681.887 23.041c0 7.632 4.608 12.096 9.936 12.096 5.04 0 8.784-4.032 8.784-8.784 0-4.752-3.312-8.208-7.632-8.208-.864 0-2.016.144-2.304.288.72-4.896 5.328-10.656 9.936-13.536L13.415.001zm24.768 0c-7.2 5.184-12.384 13.68-12.384 23.04 0 7.632 4.608 12.096 9.936 12.096 4.896 0 8.784-4.032 8.784-8.784 0-4.752-3.456-8.208-7.776-8.208-.864 0-1.872.144-2.16.288.72-4.896 5.184-10.656 9.792-13.536L38.183.001z" />
           </svg>
           {typeof testimonial.content === 'string' ? (
-            <p className="text-gray-900 dark:text-gray-900">{testimonial.content}</p>
+            <p className="text-gray-900 dark:text-gray-300">{testimonial.content}</p>
           ) : (
             testimonial.content
           )}
@@ -189,7 +189,7 @@ export function Testimonials() {
       onMouseLeave={() => setDuration(150)}
     >
       <div
-        className="pointer-events-none absolute right-0 bottom-1/2 left-0 bg-gradient-to-t from-gray-100 dark:from-gray-800"
+        className="pointer-events-none absolute right-0 bottom-1/2 left-0 bg-gradient-to-t from-gray-100 dark:from-gray-900"
         style={{ height: 607, maxHeight: '50vh' }}
       />
       <div className="-my-8 flex overflow-hidden">
