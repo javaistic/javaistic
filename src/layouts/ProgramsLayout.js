e4dc24c39a67c60185943e5e92517d3e3f0998a5
Arghya Ghosh <uiuxarghya@gmail.com>
2024-01-10 15:19:02 +0000

    feat: update assests url

--- src/layouts/ProgramsLayout.js
+++ src/layouts/ProgramsLayout.js
@@ -22,10 +22,10 @@ export function ProgramsLayout(props) {
           property="og:image"
           content={
             router.pathname === '/programs'
-              ? `https://og-image-javaistic.vercel.app/.png?theme=light&md=1&fontSize=100px&images=https%3A%2F%2Fassets.javaistic.ml%2Flogo%2Fjavaistic-programs-main.svg`
+              ? `https://og-image-javaistic.vercel.app/.png?theme=light&md=1&fontSize=100px&images=https%3A%2F%2Fassets-javaistic.vercel.app%2Flogo%2Fjavaistic-programs-main.svg`
               : `https://og-image-javaistic.vercel.app/**${
                   props.layoutProps.meta.metaTitle || props.layoutProps.meta.title
-                }**.png?theme=light&md=1&fontSize=120px&images=https%3A%2F%2Fassets.javaistic.ml%2Flogo%2Fjavaistic-programs-main.svg`
+                }**.png?theme=light&md=1&fontSize=120px&images=https%3A%2F%2Fassets-javaistic.vercel.app%2Flogo%2Fjavaistic-programs-main.svg`
           }
         />
         <meta key="twitter:card" name="twitter:card" content="summary_large_image" />
@@ -34,10 +34,10 @@ export function ProgramsLayout(props) {
           name="twitter:image"
           content={
             router.pathname === '/programs'
-              ? `https://og-image-javaistic.vercel.app/.png?theme=light&md=1&fontSize=100px&images=https%3A%2F%2Fassets.javaistic.ml%2Flogo%2Fjavaistic-programs-main.svg`
+              ? `https://og-image-javaistic.vercel.app/.png?theme=light&md=1&fontSize=100px&images=https%3A%2F%2Fassets-javaistic.vercel.app%2Flogo%2Fjavaistic-programs-main.svg`
               : `https://og-image-javaistic.vercel.app/**${
                   props.layoutProps.meta.metaTitle || props.layoutProps.meta.title
-                }**.png?theme=light&md=1&fontSize=120px&images=https%3A%2F%2Fassets.javaistic.ml%2Flogo%2Fjavaistic-programs-main.svg`
+                }**.png?theme=light&md=1&fontSize=120px&images=https%3A%2F%2Fassets-javaistic.vercel.app%2Flogo%2Fjavaistic-programs-main.svg`
           }
         />
       </Head>
