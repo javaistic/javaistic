449f4958d63074748031a8bfc8891c804ad13bd9
Arghya Ghosh <71373838+uiuxarghya@users.noreply.github.com>
2025-05-13 04:26:51 +0530

    BREAKING CHANGE: cleanup old project code

--- .eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  extends: 'next',
-  rules: {
-    'react/react-in-jsx-scope': 'off',
-    'react/display-name': 'off',
-    'react-hooks/exhaustive-deps': 'off',
-    'jsx-a11y/anchor-is-valid': 'off',
-  },
-}
