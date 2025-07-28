95cd11a3706a4fb2aeae761b434533a8830505c1
Arghya Ghosh <71373838+uiuxarghya@users.noreply.github.com>
2025-07-29 04:15:53 +0530

    feat(page): enhance homepage layout with new sections and buttons

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -1,23 +1,20 @@
-import type { ReactNode } from "react";
-import { HomeLayout } from "fumadocs-ui/layouts/home";
 import { baseOptions } from "@/app/layout.config";
+import { HomeLayout } from "fumadocs-ui/layouts/home";
+import type { ReactNode } from "react";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <HomeLayout
-      {...baseOptions}
-      links={[
-        {
-          text: "Documentation",
-          url: "/docs",
-        },
-        {
-          text: "Programs",
-          url: "/programs",
-        },
-      ]}
-    >
-      {children}
-    </HomeLayout>
+    <>
+      <HomeLayout
+        {...baseOptions}
+        style={
+          {
+            "--spacing-fd-container": "1280px",
+          } as object
+        }
+      >
+        {children}
+      </HomeLayout>
+    </>
   );
 }
